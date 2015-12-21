70cb85bb1528292494539e869e218ba8d25f9708
Demhunt <grigorenkodima@gmail.com>
2015-12-21 21:47:28 +0300

    Create class Example

---
+++ Example.java
@@ -0,0 +1,12 @@
+/*Это простая программа на Java .
+        Присвоить исходному файлу имя
+        "Example.java"
+*/
+public class Example {
+// Эта программа начинается с вызова метода main ()
+    public static void main(String args[]) {
+        System.out.println("Пpocтaя программ на Java.");
+    }
+}
+
+
